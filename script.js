011a2a5e7f23cad4e0648617f788f6571546efb5
joshuapinish <dianaandreateo@gmail.com>
2026-01-02 21:37:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ fetch("posts.json")
       const article = document.createElement("article");
       article.className = "post";
 
-      article.innerHTML = `
-        <h2>${p.title}</h2>
-        <span>${p.date}</span>
-        <p>${p.content}</p>
-      `;
+article.innerHTML = `
+  <h2>
+    <a href="post.html?id=${p.id}">
+      ${p.title}
+    </a>
+  </h2>
+  <span>${p.date}</span>
+  <p>${p.content}</p>
+`;
+
 
       container.appendChild(article);
     });
